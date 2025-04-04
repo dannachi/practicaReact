be058afc7e857cce3e64495acd383d012f594aef
Daniel Rodriguez <daniel.rodriguez.sanchez@correounivalle.edu.co>
2025-04-04 10:07:54 -0500

    Ejercicios primer nivel react
    Ejercicios primer nivel react

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,71 @@
 import React from "react";
 import "./Layout.css"; // Importamos los estilos
+import { useState} from "react";
 
-export default function Layout() {
-  return (
-    <div className="container">
-      {/* Header */}
-      <header className="header">Título de la aplicación</header>
+const saludo = "Hola Daniel, sos el más grande desarrollador frontEnd que jamas haya existido, alto capo!";
+const nombre = "Daniel Rodriguez Sanchez"
+const frutas = ["Manzana", "Banano", "Pera", "Guanabana", "Cereza", "Mango"]
+const tareas = ["Revisar correos", "Automatizar tareas", "Realizar refinamiento de proyecto","Asistir a obra de teatro", "Correr una maraton"]
 
-      <div className="content-wrapper">
-        {/* Contenido principal */}
-        <main className="main-content">
-          <div className="box">Elemento 1</div>
-          <div className="box">Elemento 2</div>
-          <div className="box">Elemento 3</div>
-        </main>
+function Usuario() {
+  return <h1>{nombre}</h1>; // Se usa { } para insertar JS dentro de JSX
+}
 
-        {/* Sidebar (barra lateral) */}
-        <aside className="sidebar">Menú lateral</aside>
+function Producto(props){
+  return <p>Hola, el {props.nombre} vale {props.precio} la libra!</p>
+}
+
+function Contador(){
+  const [contador, setContador]= useState(0);
+  const [descontador, setDescontador] = useState(0);
+
+  return(
+    <>
+      <div>
+          <p>Contador:{contador}</p>
+          <button onClick={() => setContador(contador + 1)}>Incrementar</button>
       </div>
-    </div>
-  );
+      <div>
+          <p>Descontador:{descontador}</p>
+          <button onClick={() => setDescontador(descontador -1)}>Decrementar</button>
+      </div>
+    </>
+  )
+}
+
+function Mensaje({ usuario }){
+  return <p>{usuario ? "Bienvenido" : "Inicia sesión"}</p>
+}
+
+function ListaFrutas(){
+  return(
+    <ul>
+      {frutas.map((fruta, index) =>(
+        <li key={index}> {fruta} </li>
+      ))}
+    </ul>
+  )
+}
+
+function ListaTareas(){
+  return(
+    <ul>
+      {tareas.map((tareas, index) =>(
+        <li key={index}>{tareas}</li>
+      ))}
+
+    </ul>
+  )
+}
+
+function Evento(){
+  const darClick = () => {
+    alert("¡El que lo lea es gay!");
+  };
+  return <button onClick={darClick}>Haz clic</button>
+
+}
+
+export default function App(){
+  return <Evento />;
 }
